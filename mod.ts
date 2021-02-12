4550dd6ec689029651aa2638ac7982d3b7a7bc16
eliassjogreen <eliassjogreen1@gmail.com>
2021-02-12 22:27:04 +0100

    feat: export stuff

--- mod.ts
+++ mod.ts
@@ -1,1 +1,8 @@
 export * from "./src/angle.ts";
+export * from "./src/matrix2.ts";
+export * from "./src/matrix3.ts";
+export * from "./src/matrix4.ts";
+// export * from "./src/projection.ts";
+export * from "./src/vector2.ts";
+export * from "./src/vector3.ts";
+export * from "./src/vector4.ts";
--- test.ts
+++
@@ -1,9 +0,0 @@
-import { Matrix2 } from "./src/matrix2.ts";
-
-const v = new Matrix2();
-
-v[0][0] = 11;
-
-console.log(v);
-
-// console.log(v.cross(new Vector2(1, 1)));
